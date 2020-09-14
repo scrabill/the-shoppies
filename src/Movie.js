18d0bce198010fe71f964b1f72b3db1956dde55f
scrabill <shannon.crabill@gmail.com>
2020-09-14 17:57:15 -0400

    Adds Movie component to conditionally render movie in nomination or results list

---
+++ src/Movie.js
@@ -0,0 +1,31 @@
+import React from 'react'
+
+export default class Movie extends React.Component {
+
+    // handleOnClick(e) {
+    //     e.preventDefault();
+    //     let id = e.target.id
+    //     this.setState(prevState => ({
+    //         nominees: [...prevState.nominees, id]
+    //       }))
+      
+    // }
+
+    render() {
+
+        let disabled = ""
+
+        if (this.props.nomineeIDs.length >= 5 && this.props.text === "Nominate") {
+            disabled = true
+        }
+
+        if (this.props.nomineeIDs.includes(this.props.movie.imdbID) === true && this.props.text === "Nominate") {
+            disabled = true
+        }
+        return (
+            <li id={this.props.movie.imdbID}>{this.props.movie.Title} - {this.props.movie.Year} - <button id={this.props.movie.imdbID} onClick={(e) => this.props.onClick(e)} disabled={disabled}>{this.props.text}</button></li>
+        )
+            
+    }
+
+}
